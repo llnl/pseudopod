f0b27e9d2c38947c1b1e4917ac5d86b306cdbe03
Jordan Ogas <jogas@lanl.gov>
2026-03-05 16:23:43 -0500

    brain dump

--- src/handlers/virtid.h
+++ src/handlers/virtid.h
@@ -1,13 +1,30 @@
 // Copyright (c) Lawrence Livermore National Security, LLC and other Pseudopod Contributors. See top-level LICENSE and COPYRIGHT files for dates and other details.
 // SPDX-License-Identifier: (Apache-2.0)
 
-#ifndef LIBPSEUDO_VIRTSETID_H
-#define LIBPSEUDO_VIRTSETID_H
+#ifndef LIBPSEUDO_VIRTID_H
+#define LIBPSEUDO_VIRTID_H
 #include <pseudo/pseudo.h>
-#include "containers.h"
+#include "internal/containers.h"
 
 #define ID_UNCHANGED 0xFFFFFFFF
 
-void virtid_attach_handlers(pseudo_config_t* cfg, idtrack_t* id_states);
+typedef struct virtid virtid_t;
 
-#endif // LIBPSEUDO_VIRTSETID_H
+typedef struct {
+    pseudo_cb_t parent;  // parent_cb_func_t
+    pseudo_cb_t tracer;  // tracer_cb_func_t
+    pseudo_cb_t syscall; // syscall_cb_func_t
+} virtid_callbacks_t;
+
+// create && delete virtid context
+virtid_t* virtid_init(const id_state_t* base_id); // base_id can be NULL (will use default)
+void virtid_free(virtid_t* v);
+
+// set/get base id
+void virtid_set_id(virtid_t* v, const id_state_t* base_id);
+const id_state_t* virtid_get_id(const virtid_t* v);
+
+// return the callbacks for this virtid instance.
+virtid_callbacks_t virtid_callbacks(virtid_t* v);
+
+#endif // LIBPSEUDO_VIRTID_H
--- src/libpseudo/pseudo.c
+++ src/libpseudo/pseudo.c
@@ -44,6 +44,7 @@ int pseudo_run(pseudo_config_t* pseudo_cfg) {
 
     DEBUG(stderr, "pseudo_run: clone succeded\n");
 
+    // parent callbacks (user-attached, including virtid_parent_cb if enabled)
     for (int i = 0; i < cfg->cbs.len; i++) {
         DEBUG(stderr, "pseudo_run: executing callback %d\n", i);
         void* cb_args = cfg->cbs.callbacks[i].cbargs;
@@ -55,20 +56,8 @@ int pseudo_run(pseudo_config_t* pseudo_cfg) {
 
     DEBUG(stderr, "pseudo_run: parent callback succeded\n");
 
-    if (cfg->virt_enabled) {
-        idtrack_t* id_states = get_id_tracker();
-        virtid_attach_handlers(pseudo_cfg, id_states);
-        unshare_id_state(id_states, getpid(), child);
-
-        handle_events(child, pseudo_cfg);
-    } else {
-        int status;
-        if (waitpid(child, &status, WUNTRACED) == -1) { die("waitpid initial"); }
-        continue_child(child);
-        DEBUG(stderr, "pseudo_run: waiting for child exit\n");
-        waitpid(child, &status, 0);
-
-    }
+    // Always run the event loop.
+    handle_events(child, pseudo_cfg);
     DEBUG(stderr, "pseudo_run: done\n");
 
     return 0;
